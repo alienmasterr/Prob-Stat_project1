b5419dd63dd5547f176828a5c2e2c665b69c3825
alina <alina230605@gmail.com>
2025-02-15 22:12:02 +0200

    dataset description

--- Ukraine.R
+++ Ukraine.R
@@ -1,10 +1,28 @@
 library(tidyverse)
 #-------------------------------------------------------------------------------
-#Introduction and data
+
+#Introduction and data:
+
+#Source:
+#https://www.kaggle.com/datasets/livochka/youth-opinion-ukraine-2002-vs-2019/data
+
+#Description:
+#Statistical analysis and insights from National survey of young people 2002 vs personal (author's, not mine) survey 2019.
+#This dataset contains sociopolitical and demographic survey data conducted among Ukrainian youth
+#with a focus on public opinions, personal characteristics and political preferences.
+
+# *you may find a very detailed variables analysis by following the link above
+
+#-------------------------------------------------------------------------------
+
+#Research question:
 
 #-------------------------------------------------------------------------------
 
 #Hypotheses:
+
+#h0:
+#h1:
 
 #-------------------------------------------------------------------------------
 
@@ -14,3 +32,10 @@ df_ua
 
 df_ua %>% colnames()
 df_ua %>% summary()
+
+#-------------------------------------------------------------------------------
+
+#Data cleaning
+
+#-------------------------------------------------------------------------------
+
